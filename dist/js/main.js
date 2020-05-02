235c742710217a55048d14c13f27ba6b3f06c5d9
Justin <justinzhao@zahos-macbook-pro.local>
2020-05-02 16:31:57 -0700

    created pricing page

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,37 +2,51 @@
 var textWrapper = document.querySelector('.ml12');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({
-    loop: false
-  })
-  .add({
-    targets: '.ml12 .letter',
-    translateX: [40, 0],
-    translateZ: 0,
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 1200,
-    delay: (el, i) => 500 + 30 * i
-  })
+function loadTitle() {
+  anime.timeline({
+      loop: false
+    })
+    .add({
+      targets: '.ml12 .letter',
+      translateX: [40, 0],
+      translateZ: 0,
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      duration: 1200,
+      delay: (el, i) => 500 + 30 * i
+    })
 
-var textWrapper2 = document.querySelector('.ml13');
-textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  var textWrapper2 = document.querySelector('.ml13');
+  textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({
-    loop: false
-  })
-  .add({
-    delay: 1200
-  })
-  .add({
-    targets: '.ml13 .letter',
-    translateY: [100, 0],
-    translateZ: 0,
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 1400,
-    delay: (el, i) => 300 + 30 * i
-  });
+  anime.timeline({
+      loop: false
+    })
+    .add({
+      delay: 1200
+    })
+    .add({
+      targets: '.ml13 .letter',
+      translateY: [100, 0],
+      translateZ: 0,
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      duration: 1400,
+      delay: (el, i) => 300 + 30 * i
+    });
+}
 
-  //scroll reveal
-  ScrollReveal().reveal('.ui.medium.image', {delay: 500});
+//scroll reveal
+ScrollReveal().reveal('.ui.medium.image', {
+  delay: 500
+});
+
+//swup js
+const swup = new Swup({
+  theme: [new SwupSlideTheme()]
+});
+
+loadTitle();
+
+// this event runs for every page view after initial load
+swup.on('contentReplaced', loadTitle);
