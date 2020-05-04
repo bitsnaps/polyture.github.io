14b3c7e3bfc500dc388132d514b653abf64c4b72
Justin Zhao <40444147+zhaojustin@users.noreply.github.com>
2020-05-03 18:43:33 -0700

    removed swup and finished contact page

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,53 +2,46 @@
 var textWrapper = document.querySelector('.ml12');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-function loadTitle() {
 
-  AOS.init();
+anime.timeline({
+  loop: false
+})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  })
 
-  anime.timeline({
-      loop: false
-    })
-    .add({
-      targets: '.ml12 .letter',
-      translateX: [40, 0],
-      translateZ: 0,
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 1200,
-      delay: (el, i) => 500 + 30 * i
-    })
+var textWrapper2 = document.querySelector('.ml13');
+textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-  var textWrapper2 = document.querySelector('.ml13');
-  textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+anime.timeline({
+  loop: false
+})
+  .add({
+    delay: 1200
+  })
+  .add({
+    targets: '.ml13 .letter',
+    translateY: [100, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 300 + 30 * i
+  });
 
-  anime.timeline({
-      loop: false
-    })
-    .add({
-      delay: 1200
-    })
-    .add({
-      targets: '.ml13 .letter',
-      translateY: [100, 0],
-      translateZ: 0,
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 1400,
-      delay: (el, i) => 300 + 30 * i
-    });
-}
 
-//swup js
-const swup = new Swup();
-
-loadTitle();
-
-// this event runs for every page view after initial load
-swup.on('contentReplaced', loadTitle);
-
-$("#get-access-button").click(function() {
+$("#get-access-button").click(function () {
   $([document.documentElement, document.body]).animate({
-      scrollTop: $("#early-access").offset().top
+    scrollTop: $("#early-access").offset().top
   }, 1000);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init();
 });
