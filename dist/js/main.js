052a1c1dd1d052dae94687cb727f441b10d65526
Justin Zhao <40444147+zhaojustin@users.noreply.github.com>
2020-07-05 18:37:24 -0700

    finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -36,42 +36,6 @@ function hideAllTabs() {
     }
 }
 
-var brea_map;
-var berkeley_map;
-
-function initMap() {
-    berkeley_map = L.map('berkeley-map', {
-        center: [37.870, -122.268],
-        zoom: 12,
-        zoomControl: false,
-        scrollWheelZoom: false
-    });
-    brea_map = L.map('brea-map', {
-        center: [33.908, -117.858],
-        zoom: 12,
-        zoomControl: false,
-        scrollWheelZoom: false
-    });
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoianVzdGluejAyIiwiYSI6ImNrYzU4b24zcDAxNDgycXFwN2wyOTYxZW8ifQ.gzuxqR7Ja6jzKHcSh4SNww'
-    }).addTo(brea_map);
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoianVzdGluejAyIiwiYSI6ImNrYzU4b24zcDAxNDgycXFwN2wyOTYxZW8ifQ.gzuxqR7Ja6jzKHcSh4SNww'
-    }).addTo(berkeley_map);
-}
-
 
 //sidebar toggle
 function toggleSidebar() {
